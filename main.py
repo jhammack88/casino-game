1caa61774fbe3430ad826d407f67eaecc230a292
jhammack88 <johnhammack88@gmail.com>
2019-09-24 13:37:59 -0600

    new- main.py

--- main.py
+++ main.py
@@ -19,7 +19,19 @@ def spin_wheel(slot_options):
     
     reels = [reel1[0], reel2[0], reel3[0]]
     print(reels)
+    return reels
+reels = spin_wheel(slot_options)
 
-spin_wheel(slot_options)
+def winning(reels):
+    if reels[0] == reels[1] and reels[2] == reels[1]:
+        winnings = 5
+    elif reels[0] == reels[1] or reels[2] == reels[0] or reels[1] == reels[2]:
+        winnings = 3
+    else: 
+        winnings = 0
+    print(winnings)
+    return winnings
 
+    
 
+winning(reels)
