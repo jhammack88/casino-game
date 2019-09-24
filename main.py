74b61f38fe4eca34bf989fbb5e8c8cd7a2b365a6
jhammack88 <johnhammack88@gmail.com>
2019-09-24 14:01:18 -0600

    new- main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 print("Howdy! Welcome to Cowboy Slots!")
 
-user_wallet = 100
+user_wallet = 0
 
 slot_options = {
     "7" : 10,
@@ -12,33 +12,47 @@ slot_options = {
     "Apple" : 7
 }
 
-def spin_wheel(slot_options):
+def spin_wheel(slot_options, bet):
+    global user_wallet
     reel1 = random.choices(list(slot_options.keys()), list(slot_options.values()))
     reel2 = random.choices(list(slot_options.keys()), list(slot_options.values()))
     reel3 = random.choices(list(slot_options.keys()), list(slot_options.values()))
     
     reels = [reel1[0], reel2[0], reel3[0]]
+    user_wallet -= bet
     print(reels)
     return reels
-reels = spin_wheel(slot_options)
+
 
 def winning(reels):
+    global user_wallet
     if reels[0] == reels[1] and reels[2] == reels[1]:
         winnings = 5
     elif reels[0] == reels[1] or reels[2] == reels[0] or reels[1] == reels[2]:
         winnings = 3
     else: 
         winnings = 0
+    user_wallet += winnings    
     print(winnings)
     return winnings
 
-winning(reels)
 
 def wallet():
     global user_wallet
     add_money = float(input("how much would you like to add? "))
     user_wallet += add_money
+
+
+
 wallet()
+
+bet = float(input("How much would you like to bet?"))
+
+def nav():
+    
+
+reels = spin_wheel(slot_options, bet)
+winning(reels)
 
 print(user_wallet)
 
