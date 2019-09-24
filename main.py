0f113e1c2417e88396f56e5f847d0b917a6c004d
jhammack88 <johnhammack88@gmail.com>
2019-09-24 11:06:40 -0600

    modified main.py

--- main.py
+++ main.py
@@ -19,4 +19,19 @@ def play():
         slotTwo = spinWheel()
         slotThree = spinWheel()
         printScore()
-        
+        playQuestion = askPlayer()
+
+def askPlayer():
+    global stake
+    while(True):
+        answer = input("You have $" + str(stake) + ". Would you like to play? ")
+        print("Please only use y or n to answer")
+        answer = answer.lower()
+        if(answer == "y"):
+            return True
+        elif(answer == "n"):
+            print("This cowpokes ending balance is $" + str(stake) + ".")
+            return False
+        else:
+            print("only y or n is accepted.")
+
