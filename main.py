a80099b0d9b3a90ba08dc218f0f8cff818000673
jhammack88 <johnhammack88@gmail.com>
2019-09-24 13:21:26 -0600

    new- main.py

--- main.py
+++ main.py
@@ -1,37 +1,25 @@
 import random
-
 print("Howdy! Welcome to Cowboy Slots!")
 
 balance = 100
 
-ITEMS = ["BOOT", "HAT", "GUN", "SADDLE", "LASSO", "GOLD"]
+slot_options = {
+    "7" : 10,
+    "777" : 1,
+    "Cherry": 8,
+    "Grape" : 10,
+    "Orange" : 5,
+    "Apple" : 7
+}
 
-slotOne = None
-slotTwo = None
-slotThree = None
-stake = balance
+def spin_wheel(slot_options):
+    reel1 = random.choices(list(slot_options.keys()), list(slot_options.values()))
+    reel2 = random.choices(list(slot_options.keys()), list(slot_options.values()))
+    reel3 = random.choices(list(slot_options.keys()), list(slot_options.values()))
+    
+    reels = [reel1[0], reel2[0], reel3[0]]
+    print(reels)
 
-def play():
-    global stake, slotOne, slotTwo, slotThree
-    playQuestion = askPlayer()
-    while(stake != 0 and playQuestion == True):
-        slotOne = spinWheel()
-        slotTwo = spinWheel()
-        slotThree = spinWheel()
-        printScore()
-        playQuestion = askPlayer()
+spin_wheel(slot_options)
 
-def askPlayer():
-    global stake
-    while(True):
-        answer = input("You have $" + str(stake) + ". Would you like to play? ")
-        print("Please only use y or n to answer")
-        answer = answer.lower()
-        if(answer == "y"):
-            return True
-        elif(answer == "n"):
-            print("This cowpokes ending balance is $" + str(stake) + ".")
-            return False
-        else:
-            print("only y or n is accepted.")
 
