844fac588989dd95e744008a0dff719df5c011e0
jhammack88 <johnhammack88@gmail.com>
2019-09-24 10:09:05 -0600

    modified main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import random
 
 print("Howdy! Welcome to Cowboy Slots!")
-print("Your current balance is $100\n")
+# print("Your current balance is $100\n")
 
 balance = 100
 
 ITEMS = ["BOOT", "HAT", "GUN", "SADDLE", "LASSO", "GOLD"]
 
+slotOne = None
+slotTwo = None
+slotThree = None
+stake = balance
+
+def play():
+    global stake, slotOne, slotTwo, slotThree
+    playQuestion = askPlayer()
+    while(stake != 0 and playQuestion == True):
+        slotOne = spinWheel()
+        slotTwo = spinWheel()
+        slotThree = spinWheel()
+        printScore()
+        playQuestion = askPlayer()
