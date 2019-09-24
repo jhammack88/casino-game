7dbd6c1db0d124842122d6366a1f2ffb5e92e85b
jhammack88 <johnhammack88@gmail.com>
2019-09-24 15:23:39 -0600

    new- main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import random
 
 slot_options = {
-    "7" : 10,
+    "7" : 3,
     "777" : 1,
-    "Cherry": 8,
-    "Grape" : 10,
-    "Orange" : 5,
-    "Apple" : 7
+    "CowboyHat": 8,
+    "Pistol" : 10,
+    "Saddle" : 5,
+    "Lasso" : 7
 }
 
 def spin_wheel(slot_options, bet):
@@ -22,31 +22,33 @@ def spin_wheel(slot_options, bet):
         reels = [reel1[0], reel2[0], reel3[0]]
         user_wallet -= bet
         print(reels)
-        winning(reels)
+        winning(reels, slot_options)
         return reels
 
 
-def winning(reels):
+def winning(reels, slot_options):
     global user_wallet
     if reels[0] == reels[1] and reels[2] == reels[1]:
-        winnings = 5
-    elif reels[0] == reels[1] or reels[2] == reels[0] or reels[1] == reels[2]:
-        winnings = 3
+        winnings = 10 * (1 / slot_options[reels[0]])
+    elif reels[0] == reels[1] or reels[2] == reels[0]:
+        winnings = 7 * (1 / slot_options[reels[0]])
+    elif reels[1] == reels[2]:
+        winnings = 7 * (1 / slot_options[reels[1]])
     else: 
         winnings = 0
     user_wallet += winnings    
-    print(f"${winnings}")
+    print(f"${winnings:.2f}")
     print(f"${user_wallet}")
     nav()
     return winnings
 
 def nav():
     global user_wallet
-    user_choice = input("what would you like to do? \n 1: Spin Wheel \n 2: Check Balance \n 3: Cash Out \n")
+    user_choice = input("What would you like to do? \n 1: Spin Wheel \n 2: Check Balance \n 3: Cash Out \n")
     if user_choice == "1":
         reels = spin_wheel(slot_options, bet)
     elif user_choice == "2":
-        print(f"your current balance is.. ${user_wallet}")
+        print(f"Your current balance is.. ${user_wallet}")
         nav()
     elif user_choice == "3":
         print(f"Thanks! Your final balance is.. ${user_wallet}") 
