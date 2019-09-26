002e63f2d43d6f76984ea5efe65ce799105a46bd
jhammack88 <johnhammack88@gmail.com>
2019-09-26 09:46:44 -0600

    new- main.py

--- main.py
+++ main.py
@@ -30,39 +30,45 @@ def winning(reels, slot_options):
     global user_wallet
     if reels[0] == reels[1] and reels[2] == reels[1]:
         winnings = 300 * (.5 / slot_options[reels[0]])
+        print(f"You WIN!! ...${winnings:.2f}")
     elif reels[0] == reels[1] or reels[2] == reels[0]:
         winnings = 200 * (1 / slot_options[reels[0]])
+        print(f"You WIN!! ...${winnings:.2f}")
     elif reels[1] == reels[2]:
         winnings = 100 * (1 / slot_options[reels[1]])
+        print(f"You WIN!! ...${winnings:.2f}")
     else: 
         winnings = 0
-    user_wallet += winnings    
-    print(f"You WIN!! ...${winnings:.8f}")
-    print(f"Your new balance is ...${user_wallet}")
+        print(f"Sorry you booty!")
+    user_wallet += winnings  
+     
+    
+    print(f"Your new balance is ...${user_wallet:.2f}")
     nav()
     return winnings
 
 def nav():
     global user_wallet
-    user_choice = input("What would you like to do? \n 1: Spin Wheel \n 2: Check Balance \n 3: Add Funds \n 4: Cash Out")
+    user_choice = input("What would you like to do? \n 1: Spin Wheel \n 2: Check Balance \n 3: Add Funds \n 4: Cash Out\n")
     if user_choice == "1":
         reels = spin_wheel(slot_options, bet)
     elif user_choice == "2":
-        print(f"Your current balance is.. ${user_wallet}")
+        print(f"Your current balance is.. ${user_wallet:.2f}")
         nav()
     elif user_choice == "3":
-        input("How much would you like to add?")
+        wallet()
     elif user_choice == "4":
-        print(f"Thanks! Your final balance is.. ${user_wallet}") 
+        print(f"Thanks! Your final balance is.. ${user_wallet:.2f}") 
         exit()
 
     
 
 def wallet():
     global user_wallet
-    add_money = input("How much would you like to add? $")
+    add_money = input("How much would you like to add?\n $")
     if add_money.isnumeric() == True:
         user_wallet += float(add_money)
+        print(f"Your new balance is... ${user_wallet:.2f}")
         nav()
     else: 
         print("Invalid Input")
@@ -70,8 +76,8 @@ def wallet():
     
 
 print("Howdy! Welcome to Cowboy Slots!")
-user_wallet = float(input("How much would you like to add? $"))
-bet = float(input("How much would you like to bet? $"))
+user_wallet = float(input("How much would you like to add?\n $"))
+bet = float(input("How much would you like to bet?\n $"))
 reels = spin_wheel(slot_options, bet)
 
 
